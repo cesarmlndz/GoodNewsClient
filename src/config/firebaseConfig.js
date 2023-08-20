4dbb928d7da5fd644fd70e78a3b75c9687056939
Cesar Melendez <cesarf.melendez@gmail.com>
2023-08-20 19:30:41 -0400

    client changes

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth"
 console.log(process.env.REACT_APP_API_KEY.slice(1, -2))
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY.slice(1, -2),
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN.slice(1, -2),
-  projectId: process.env.REACT_APP_PROJECT_ID.slice(1, -2),
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET.slice(1, -2),
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID.slice(1, -2),
-  appId: process.env.REACT_APP_APP_ID.slice(1, -2),
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID.slice(1, -2)
+  apiKey: "AIzaSyCPz_NVx7K2bT3jYGmEZN7SL3nxdY_4WQU",
+  authDomain: "goodnewsx-87b94.firebaseapp.com",
+  projectId: "goodnewsx-87b94",
+  storageBucket: "goodnewsx-87b94.appspot.com",
+  messagingSenderId: "898998222440",
+  appId: "1:898998222440:web:839b66660fd5856967c5dd",
+  measurementId: "G-95ZXXT30S2"
 };
 
 const app = initializeApp(firebaseConfig);
